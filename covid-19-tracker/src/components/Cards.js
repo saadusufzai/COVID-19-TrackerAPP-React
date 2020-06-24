52947e00b963ee4830936a346f1fa9ebb5a963e6
Saad Aslam <saadusufzai@gmail.com>
2020-06-24 12:58:21 +0500

    1 Card Set

--- covid-19-tracker/src/components/Cards.js
+++ covid-19-tracker/src/components/Cards.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import Card from 'material';
-import Typography from ''
+import { Card, CardContent, Typography, Grid, StylesProvider} from '@material-ui/core'
+import CountUp from 'react-countup'
+import cx from 'classnames'
 export default function Cards() {
     return (
-        <div>
-            <Card variant="outlined">
+        <div className='container'>
+            <Grid container spacing={3} justify='center'>
+                <Grid item component={Card} xs={3} className='card' >
+                    <CardContent className='cards' variant="outlined">
+                    <Typography color='textSecondary' gutterBottom >Recovered</Typography>
+                    <CountUp start={0} end={10000} duration={2} seperator=',' />
+                    <Typography >{ new Date().toDateString()}</Typography>
+                    </CardContent>
+                </Grid>
 
-            </Card>
+            </Grid>
+           
         </div>
     )
 }
