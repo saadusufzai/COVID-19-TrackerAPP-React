20dae8aa20d5a72b45fb703ade1e3da4c59e6d0c
Saad Aslam <saadusufzai@gmail.com>
2020-06-26 04:27:07 +0500

    Horizontal Bar Graph  done
    Styling remaining

--- covid-19-tracker/src/components/Charts/Chart.js
+++ covid-19-tracker/src/components/Charts/Chart.js
@@ -1,28 +1,46 @@
 import React, {useState,useEffect} from 'react'
 import styles from './Charts.module.css'
-import {fetchData} from '../../api/api'
-import { Line, Bar } from 'react-chartjs-2';
+//import {fetchData} from '../../api/api'
+import { Line, Bar,Pie,HorizontalBar } from 'react-chartjs-2';
 
 
 
 export default function Chart({data:{confirmed,deaths,recovered,lastUpdate}}) {
-    const[data, setData] = useState([])
-    useEffect(() => {
-       const fetchAPI = async ()=>{ 
-        const fetchedData = await fetchData()
+    // const[data, setData] = useState([])
+    // useEffect(() => {
+    //    const fetchAPI = async ()=>{ 
+    //     const fetchedData = await fetchData()
         
-        setData(fetchedData)
-        }
-        fetchAPI()
-    }, [])
-
-    console.log(data)
+    //     setData(fetchedData)
+    //     }
+    //     fetchAPI()
+    // }, [])
+    if(!deaths){
+        return 'loading...'
+    }
+   
     // console.log(deaths.value)
     // console.log(recovered.value)
 
 
     const lineChart = (
-        <Line/>
+
+        <HorizontalBar
+            data={{
+                labels:['Confirmed','Recovered','Deaths'],
+                datasets:[{
+                    data:[confirmed.value,recovered.value,deaths.value],
+                    backgroundColor:['Yellow','green','red']
+                }],
+                
+            }}
+            options={{
+                title:{
+                    text:'COVID CASE'
+                }
+            }}
+        
+        />
     )
 
     return (
